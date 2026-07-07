16776980bb298c665cfbed680ee142eaca0290c5
keet <henry.keny@outlook.fr>
2026-07-06 22:22:36 -0400

    connection info

--- lib/http/routes/post/on_connection.dart
+++ lib/http/routes/post/on_connection.dart
@@ -1,7 +1,6 @@
 import 'package:kaouka/http/http_manager.dart';
 
 Future<dynamic> onConnection(String id) async {
-  final data = {'id': id};
-  final res = await post(data, "onConnection", withAuth: true);
+  final res = await post({}, "onConnection", withAuth: true);
   return res;
 }
--- lib/http/routes/post/on_disconnection.dart
+++ lib/http/routes/post/on_disconnection.dart
@@ -1,7 +1,6 @@
 import 'package:kaouka/http/http_manager.dart';
 
 Future<dynamic> onDisconnection(String id) async {
-  final data = {'id': id};
-  final res = await post(data, "onDisconnection", withAuth: true);
+  final res = await post({}, "onDisconnection", withAuth: true);
   return res;
 }
